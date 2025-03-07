0ac9c2b09bf9c4e5889027fcf8508eba635c5659
daniel52510 <danielblazquez640@gmail.com>
2025-03-07 15:36:06 -0500

    Update RecentProperties.js
    removed API keys

--- src/components/RecentProperties.js
+++ src/components/RecentProperties.js
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 const RecentProperties = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyAcdpax1Bk5XySYKC4xQ-6u05TnKkoMvEc",
-        authDomain: "bella-bella-realty-website.firebaseapp.com",
+        apiKey: "",
+        authDomain: "",
         projectId: "bella-bella-realty-website",
-        storageBucket: "bella-bella-realty-website.appspot.com",
-        messagingSenderId: "256708251300",
-        appId: "1:256708251300:web:b92120f36e9233b43ce031",
-        measurementId: "G-6CSM0W2QH9"
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
       };
       const app = initializeApp(firebaseConfig);
       const db = getFirestore(app);
